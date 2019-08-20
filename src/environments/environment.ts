bfa11520c5433b13e9bb976f804f7aa19b9091d3
martijnd <martijn.dorsman@gmail.com>
2019-08-21 00:43:44 +0200

    Fix linting error and firebaseconfig

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCN9dyMJaqt_PlMzUQ0oSFEk2p_9CWmuYI",
-    authDomain: "jourly-3418d.firebaseapp.com",
-    databaseURL: "https://jourly-3418d.firebaseio.com",
-    projectId: "jourly-3418d",
-    storageBucket: "",
-    messagingSenderId: "546910924781",
-    appId: "1:546910924781:web:f83e688910ca3297"
+    apiKey: 'AIzaSyCN9dyMJaqt_PlMzUQ0oSFEk2p_9CWmuYI',
+    authDomain: 'jourly-3418d.firebaseapp.com',
+    databaseURL: 'https://jourly-3418d.firebaseio.com',
+    projectId: 'jourly-3418d',
+    storageBucket: '',
+    messagingSenderId: '546910924781',
+    appId: '1:546910924781:web:f83e688910ca3297'
   }
 };
 
